edac6794133d2f87d2d8529c999035f0281dec3f
ka1peshc <kalpesh111997@gmail.com>
2021-09-08 11:09:20 +0530

    First Commit

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Contact
+    {
+        private string firstname, lastname, address, City, State, Email;
+        private int  zipNo;
+        private long Phonenumber;
+        public Contact(string fn, string ln, string addr, string city, string state, string email, long PhNo, int zip)
+        {
+            firstname = fn;
+            lastname = ln;
+            address = addr;
+            City = city;
+            State = state;
+            Email = email;
+            Phonenumber = PhNo;
+            zipNo = zip;
+        }
+
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+            Contact contact = new Contact("Kalpesh", "Chindarkar", "Sewri west", "Mumbai", "Maharashtra", "kalpesh111997@gmail.com", 9920036999, 400015);
+        }
+    }
+}
