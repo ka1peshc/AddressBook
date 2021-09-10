38b5294c471112c59978ce68fd2f11e582ffffd8
ka1peshc <kalpesh111997@gmail.com>
2021-09-10 22:48:07 +0530

    Delete record where key and firstname match

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -12,13 +12,13 @@ namespace AddressBook
         private void takeInput()
         {
             Console.WriteLine("Enter values according Firstname, Lastname, Address, City, State, Email");
-            Console.WriteLine("Zip Number, Phone number");
             Firstname = Console.ReadLine();
             Lastname = Console.ReadLine();
             Address = Console.ReadLine();
             City = Console.ReadLine();
             State = Console.ReadLine();
             Email = Console.ReadLine();
+            Console.WriteLine("Zip Number, Phone number");
             zipNo = int.Parse(Console.ReadLine());
             Phonenumber = long.Parse(Console.ReadLine());
         }
@@ -31,6 +31,48 @@ namespace AddressBook
                 Console.WriteLine("Address :{0},{1},{2},{3}", kvp.Value.address, kvp.Value.City, kvp.Value.zipNo, kvp.Value.State);
                 Console.WriteLine("Phone number: {0}", kvp.Value.Phonenumber);
                 Console.WriteLine();
+            }
+        }
+        void updateRecord()
+        {
+            Console.WriteLine("Do you want to edit name. Then type 1 or else 0");
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                Console.WriteLine("Type key");
+                string key = Console.ReadLine();
+                Console.WriteLine("Type name currently seen");
+                string currentName = Console.ReadLine();
+                Console.WriteLine("Type name you want");
+                Program p = new Program();
+                string editToName = Console.ReadLine();
+                if (p.editNameInAddressBook(key, currentName, editToName))
+                {
+                    Console.WriteLine("Successfully name updated");
+                    p.displayAddressBook();
+                }
+            }
+        }
+        void deleteRecord()
+        {
+            Console.WriteLine("Enter key you want to delete");
+            foreach (KeyValuePair<string, Contact> kvp in addressBook)
+            {
+                Console.WriteLine(kvp.Key + "  " + kvp.Value.firstname);
+            }
+            Console.WriteLine("Enter key and first name");
+            string keyName, firstname;
+            keyName = Console.ReadLine();
+            firstname = Console.ReadLine();
+            foreach (KeyValuePair<string, Contact> kvp in addressBook)
+            {
+                if (kvp.Key.Equals(keyName))
+                {
+                    if (kvp.Value.firstname.Equals(firstname))
+                    {
+                        addressBook.Remove(kvp.Key);
+                    }
+                }
             }
         }
         bool editNameInAddressBook(string key, string currentName, string editToName)
@@ -60,6 +102,7 @@ namespace AddressBook
         {
             Console.WriteLine("Welcome to Address Book");
             Program p = new Program();
+            //Enter record & display all record
             for(int i = 1; i < 3; i++)
             {
                 string temp = DateTime.Now.ToString("yyyy-MM-dd");  
@@ -70,22 +113,12 @@ namespace AddressBook
             }
             p.displayAddressBook();
 
-            Console.WriteLine("Do you want to edit name. Then type 1 or else 0");
-            int choice=int.Parse(Console.ReadLine());
-            if(choice == 1)
-            {
-                Console.WriteLine("Type key");
-                string key = Console.ReadLine();
-                Console.WriteLine("Type name currently seen");
-                string currentName = Console.ReadLine();
-                Console.WriteLine("Type name you want");
-                string editToName = Console.ReadLine();
-                if (p.editNameInAddressBook(key, currentName, editToName))
-                {
-                    Console.WriteLine("Successfully name updated");
-                    p.displayAddressBook();
-                }
-            }
+            //Editing record based on key and firstname
+            //p.updateRecord();
+
+            //Delete method invokation
+            p.deleteRecord();
+            p.displayAddressBook();
         }
     }
 }
